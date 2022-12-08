404ce4ab7c17f83e14ec4bf0f8b8da9593867e89
jisooooooooooo <dlawltn0708@naver.com>
2022-12-08 14:02:30 +0900

    CBV로 쇼핑몰 포스트 목록 페이지 만들기

--- shopping/views.py
+++ shopping/views.py
@@ -1,16 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
 
-    return render(
-        request,
-        'shopping/index.html',
-        {
-            'posts': posts,
-        }
-    )
 
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
