a604434fe345b299c5d001736d331da7292b8134
jisooooooooooo <dlawltn0708@naver.com>
2022-12-08 13:53:21 +0900

    대문 페이지와 자기소개 페이지 만들기

--- shoppingmall_prj/urls.py
+++ shoppingmall_prj/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('shopping/', include('shopping.urls')),
     path('admin/', admin.site.urls),
+    path('',include('single_pages.urls')),
 ]
