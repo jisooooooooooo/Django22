3a78b9c91d0a59d21e6f31bb9af527e9abc2bcbb
jisooooooooooo <dlawltn0708@naver.com>
2022-12-09 11:36:54 +0900

    첨부 파일 있을 경우 포스트 상세 페이지에 다운로드 버튼출력하기

--- shopping/views.py
+++ shopping/views.py
@@ -8,14 +8,5 @@ class PostList(ListView):
 class PostDetail(DetailView):
     model = Post
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
 
-    return render(
-        request,
-        'shopping/single_post_page.html',
-        {
-            'post': post,
-        }
-    )
 # Create your views here.
