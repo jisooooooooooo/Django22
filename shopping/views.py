d507277a11620fa048bb9d3933534280e101776a
jisooooooooooo <dlawltn0708@naver.com>
2022-12-16 03:57:30 +0900

    pagination으로 포스트를 나누어 보여 주기

--- shopping/views.py
+++ shopping/views.py
@@ -20,6 +20,7 @@ class CommentUpdate(LoginRequiredMixin, UpdateView):
 class PostList(ListView):
     model = Post
     ordering = '-pk'
+    paginate_by = 5
 
     def get_context_data(self, **kwargs):
         context = super(PostList, self).get_context_data()
